aafaea4ac3ad7f9424e71edd26a5d3ae568d0dbd
Johan Swan <jswannieman@gmail.com>
2024-03-17 11:09:48 +1300

    Update myApp JS Component

--- myApp.js
+++ myApp.js
@@ -161,8 +161,17 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((error, people) => {
+      if (error) {
+        return console.log(error);
+      } else {
+        done(null, people);
+      }
+    });
 };
 
 /** **Well Done !!**
